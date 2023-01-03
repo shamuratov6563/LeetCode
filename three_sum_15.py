d315de072cb56a6316cf892773124b55fb8757a5
shamuratov6563 <shamuratov6563@gmail.com>
2023-01-03 21:59:34 +0500

    threesum solution adjusted

--- three_sum_15.py
+++ three_sum_15.py
@@ -1,23 +1,35 @@
 
 
-class Solution:
+class Solution1:
     def threeSum(self, nums: list):
-
-        result = set()
+        res = []
         nums.sort()
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
+        length = len(nums)
+        for i in range(length-2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            s = set()
-            for num in nums[i + 1:]:
-                if -nums[i] - num in s:
-                    result.add(tuple([nums[i], -nums[i] - num, num]))
-                s.add(num)
-        return [i for i in result]
+            l = i+1
+            r = length-1
+            while l<r:
+                total = nums[i] + nums[l] + nums[r]
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
 
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
 
-s = Solution()
+s = Solution1()
 print(s.threeSum([-1,0,1,2,-1,-4]))
+
 
 #sorted
 #[-4,-1,-1,0,1,2]
@@ -25,4 +37,7 @@ print(s.threeSum([-1,0,1,2,-1,-4]))
 # l = 2(-1)
 # r = 4(2)
 # -1, -1, 2
+
+
+
 
